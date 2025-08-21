30dc482795f2a77a08be68126870739b4166af6b
aagargoura <ahmed.gargoura@gmail.com>
2025-08-21 18:01:48 +0100

    Add Project skeleton

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+// frontend/src/App.tsx
+export default function App() {
+  return (
+    <>
+      <h1>Hello ZK Tanit ID</h1>
+      <p style={{ textAlign: 'center' }}>
+        <img src="/zktanitid_logo.png" width={400} alt="ZK Tanit ID Logo" />
+      </p>
+    </>
+  );
+}
